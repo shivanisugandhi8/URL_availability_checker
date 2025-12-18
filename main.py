8f3ad28ed0772ff139bf5e33883ee8cd2ec40063
shivanisugandhi8 <shivanisugandhi8@gmail.com>
2025-12-18 19:02:24 +0530

    update main.py to include comments and docstrings for clarity

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+#importing necessary modules
 import requests
 import logging
 import csv
 from concurrent.futures import ThreadPoolExecutor
 import os
 
+#Building absolute file paths
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PATH = os.path.join(BASE_DIR,"URL.txt")
 RESULT_FILE = os.path.join(BASE_DIR,"result.csv")
 LOG_FILE = os.path.join(BASE_DIR,"api_calls.log")
 
+#logging setup
 logger = logging.getLogger("url_checker")
 logger.setLevel(logging.DEBUG)
 frmt = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
@@ -24,6 +27,7 @@ fh.setFormatter(frmt)
 logger.addHandler(fh)
 
 def check_url(url:str) -> tuple :
+    '''make api calls and logs the result to the file'''
     try:
         resp = requests.get(url,timeout=7)
         logger.info(f"api calling successful to {url} ,status code: {resp.status_code}")
@@ -33,6 +37,7 @@ def check_url(url:str) -> tuple :
         return url, str(e), "DOWN"
 
 def load_urls(path:str =PATH) ->list[str]:
+    '''reads urls from the file and returns them as a list'''
     with open(path,"r") as f:
         url_lst = []
         for line in f:
@@ -42,6 +47,7 @@ def load_urls(path:str =PATH) ->list[str]:
     return url_lst
 
 def save_result(result:list[tuple]) -> None:
+    '''saves the result in a csv file'''
     with open(RESULT_FILE,"a",newline="") as f:
         w = csv.writer(f)
         w.writerow(["URL","STATUS_CODE","STATE"])
